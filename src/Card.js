262911c973d118649d86546a4ac572cd9b693067
Jagadishgollapalli <77227327+Jagadishgollapalli@users.noreply.github.com>
2021-06-09 13:15:08 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -12,36 +12,42 @@ class Card extends React.Component{
       title:"NATURE",
       description:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
       tag:"View More",
+      source:'https://pixabay.com/images/search/nature/',
     }
     ,{
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs1rRlBETQmimE6HwsTxLndJMrYg88tlPnfA&usqp=CAU",
       title:"ANIMAL",
       description:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
-      tag:"View More"
+      tag:"View More",
+      source:'https://pixabay.com/images/search/animal/',
     },
     {
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjTfn4h3TtQopEjwt47kxXQh8X5U8ZB7bp34H3eKIYmEHCIkFRWrXnDKYp1rKB4RZdTKY&usqp=CAU",
       title:"VEHICLE",
       description:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
-      tag:"View More"
+      tag:"View More",
+      source:'https://pixabay.com/images/search/vehicle/',
     },
     {
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe5kk7lxn8Yuicp4TUuOFWPc7y5_RYCr37QpmdSqnOdKd6Svnl3AtURabyKiyBKrBQQaI&usqp=CAU",
       title:"BIRD",
       description:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
-      tag:"View More"
+      tag:"View More",
+      source:'https://pixabay.com/images/search/bird/',
     },
     {
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHy2FoO-inaoOTRLxfCsXYR46cAfph3Ti4TRApOyQzGOMJVhNOMdplQFBkn6IANzqoE0o&usqp=CAU",
       title:"ROBOT",
       description:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
-      tag:"View More"
+      tag:"View More",
+      source:'https://pixabay.com/images/search/robot/',
     },
     {
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4ingZa2nrU4YcA-TLGImTu82LLqNz_9cfAtRz_bhvbTCXrEzHoSpk00biyU6qSiL6pW4&usqp=CAU",
       title:"WEAPON",
       description:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
-      tag:"View More"
+      tag:"View More",
+      source:'https://pixabay.com/images/search/weapon/',
     }
     ];
     this.state={Data};
@@ -57,7 +63,7 @@ class Card extends React.Component{
         <div className="card-body">
         <h5 className="card-title">{item.title}</h5>
         <p className="card-text">{item.description}</p>
-        <a href="#" className='btn btn-dark'>{item.tag}</a>
+        <a href={item.source} className='btn btn-dark'>{item.tag}</a>
         </div>
         </div>
         </div>
